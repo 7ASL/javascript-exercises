0a7a278cf905cfe84e50a1474973066b437b627e
7ASL <202996150+7ASL@users.noreply.github.com>
2026-03-26 00:54:20 +0000

    Complete foundations exercise No. 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  const temperatureInCelsius = (temperatureInFahrenheit - 32) * (5/9);
+  return temperatureInCelsius === 0 ? 0 : Number(temperatureInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  const temperatureInFahrenheit = (temperatureInCelsius * (9/5)) + 32;
+  return temperatureInFahrenheit === 0 ? 0 : Number(temperatureInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
