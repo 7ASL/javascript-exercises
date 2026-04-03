10143eab26e8aef3d81acb65b4591674cab26c10
7ASL <202996150+7ASL@users.noreply.github.com>
2026-04-04 00:36:42 +0200

    Complete foundations exercise No. 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const str = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+const length = str.length
+for(let i = 0; i < length / 2; i++) {
+    if(str[i] !== str[length - i - 1]) return false;
+}
+return true;
 };
+
+// // Second solution
+// const palindromes = function (string) {
+// const str = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+// const rev = str.split("").reverse().join("");
+// for(let i = 0; i < str.length; i++) {
+//     if(str[i] !== rev[i]) return false;
+// }
+// return true;
+// };
 
 // Do not edit below this line
 module.exports = palindromes;
