a3c986406483f69dcd7396fe5fdcd10c6e1904e0
7ASL <202996150+7ASL@users.noreply.github.com>
2026-04-04 01:43:34 +0200

    Complete foundations exercise No. 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const currentYear = (new Date).getFullYear();
+    const age = (person) => (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+    return array.reduce((oldest, current) => age(oldest) > age(current) ? oldest : current);
 };
 
 // Do not edit below this line
