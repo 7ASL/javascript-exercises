f948325252663986283e0355b8b74870322d671d
7ASL <202996150+7ASL@users.noreply.github.com>
2026-04-04 01:03:18 +0200

    Complete foundations exercise No. 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const num = Number(n);
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
 
+    let numLast = 1;
+    let numPenult = 0;
+
+    for(let i = 2; i <= num; i++){
+        let value = numLast + numPenult;
+        numPenult = numLast;
+        numLast = value;
+    }
+
+    return numLast;
 };
 
 // Do not edit below this line
